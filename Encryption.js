a17a88b52623d8421f872e5350bbaa36e2585741
dominic <dominicwhatling@outlook.com>
2024-04-09 22:36:21 +1000

    encryption rework

--- Encryption.js
+++ Encryption.js
@@ -1,11 +1,14 @@
+import RandomNumberGenerator from "./RandomNumberGenerator.js";
+
 /**@static*/
 export default class Encryption {
 	/**@param {string} str @param {number} register*/
 	static encode(str, register) {
 		if (register < 2 || register > 36) throw new Error(`invalid register: ${register}`);
 		const cypher = Math.floor(Math.random() * (100 - 10) + 10);
-		const substiutionArr = this.#getShuffledArr(256).map(bin => this.#enforceByteSize(bin.toString(register), register));
-		return [`[${this.#enforceByteSize(cypher.toString(register), register)}]`, `{${this.#hashGen(str, cypher)}}`, `(${substiutionArr.join('')})`, ...str.split('').map((bin) => {
+		const rng = new RandomNumberGenerator(new Date().valueOf());
+		const substiutionArr = this.#getShuffledArr(256, rng).map(bin => this.#regSize(bin.toString(register), register));
+		return [`[${this.#regSize(cypher.toString(register), register)}]`, `{${this.#hashGen(str, cypher)}}`, `(${(rng.Seed * 2).toString(register)})`, ...str.split('').map((bin) => {
 			return substiutionArr[bin.charCodeAt() + cypher];
 		}).reverse()].join(' ');
 	}
@@ -14,21 +17,20 @@ export default class Encryption {
 		if (register < 2 || register > 36) throw new Error(`invalid register: ${register}`);
 		const cypher = parseInt(this.#findSubString(str, '[', ']'), register);
 		const hash = this.#findSubString(str, '{', '}');
-		const substiutionArr = this.#splitSpace(this.#findSubString(str, '(', ')'), (255).toString(register).length);
-		const finalStr = this.#splitSpace(str.split(' ').reverse().slice(0, -3), (255).toString(register).length).map((bin) => {//translate characters
+		const rng = new RandomNumberGenerator(parseInt(this.#findSubString(str, '(', ')'), register) / 2);
+		const substiutionArr = this.#getShuffledArr(256, rng).map(bin => this.#regSize(bin.toString(register), register));
+		const finalStr = str.split(' ').reverse().slice(0, -3).map((bin) => {//translate characters
 			return String.fromCharCode(substiutionArr.indexOf(bin) - cypher);
 		}).join('');
 		if (this.#hashGen(finalStr, cypher) != hash) throw new Error(`invalid hash: ${hash}`);
 		else return finalStr;
 	}
 	/**@param {string} str @param {number} register*/
-	static #enforceByteSize(str, register) {//adds zeros to stay within byte size
-		const registerLength = (255).toString(register).length;
-		while (str.length < registerLength) str = '0' + str;
-		return str;
+	static #regSize(str, register) {//add zeros to stay within register size
+		return str.padStart((255).toString(register).length, '0');
 	}
 	/**@param {string} str @param {number} seed*/
-	static #hashGen (str, seed) {
+	static #hashGen(str, seed) {
 		let h1 = 0xdeadbeef ^ seed;
 		let h2 = 0x41c6ce57 ^ seed;
 		for(let i = 0; i < str.length; i++) {
@@ -40,23 +42,15 @@ export default class Encryption {
 		return (4294967296 * (2097151 & h2) + (h1 >> 0)).toString(32);
 	}
 	/**@param {string} str @param {string} start @param {string?} end*/
-	static #findSubString(str, start, end = start) {//find math expresions within a string
+	static #findSubString(str, start, end = start) {//find expresions within a string
 		let bool = false;
 		return str.split('').map((bin) => {
 			if (bin == start || bin == end) bool = !bool;
 			else if (bool) return bin;
 		}).filter(bin => bin != null).join('');
 	}
-	/**@param {number} length*/
-	static #getShuffledArr(length) {
-		const arr = [];
-		for (let i = 0; i < length; i++) arr.push(i);
-		return arr.sort(() => (Boolean(Math.floor(Math.random() * 2))) ? 1 : -1);
-	}
-	/**@param {string} str @param {number} spaces*/
-	static #splitSpace(str, spaces) {
-		let arr = [];
-		for (let i = 0; i < str.length; i += spaces) arr.push(str.slice(i, i + spaces));
-		return arr;
+	/**@param {number} length @param {RandomNumberGenerator} rng*/
+	static #getShuffledArr(length, rng) {
+		return new Array(length).fill().map((_, i) => i).sort(() => (Boolean(Math.floor(rng.nextFloat() * 2))) ? 1 : -1);
 	}
 }
---
+++ RandomNumberGenerator.js
@@ -0,0 +1,52 @@
+//"appropriated" from https://stackoverflow.com/questions/424292/seedable-javascript-random-number-generator
+
+export default class RandomNumberGenerator {
+	#state;
+	#m;
+	#a;
+	#c;
+	#state;
+	/**@param {number} seed*/
+	constructor(seed) {
+		this.#state = seed;
+		this.#m = 0x80000000; //2^31
+		this.#a = 1103515245;
+		this.#c = 12345;
+		this.#state = seed ?? Math.floor(Math.random() * (this.#m-1));
+	}
+	get Seed() {
+		return this.#state;
+	}
+	/**@returns {number}*/
+	nextInt() {//returns a random integer
+		this.#state = (this.#a * this.#state + this.#c) % this.#m;
+		return this.#state;
+	}
+	/**@returns {number}*/
+	nextFloat() {//returns a value between 0 & 1
+		return this.nextInt() / (this.#m - 1);
+	}
+	/**@returns {number} @param {number} start @param {number} end*/
+	nextRange(start, end) {//returns a value between the two bounds
+		if (start > end) throw new Error('start is larger than end');
+		const range = end - start;
+		const randUnder1 = this.nextInt() / this.#m;
+		return start + Math.floor(randUnder1 * range);
+	}
+	/**@param {any[]} arr*/
+	choose(arr) {//returns a random element of an array
+		return arr[this.nextRange(0, arr.length)];
+	}
+	/**@returns {Generator<number>} @param {number} indices*/
+	*nextFloatArr(indices) {
+		for (let i = 0; i < indices; i ++) {
+			yield this.nextFloat();
+		}
+	}
+	/**@returns {Generator<number>} @param {number} indices*/
+	*nextIntArr(indices) {
+		for (let i = 0; i < indices; i ++) {
+			yield this.nextInt();
+		}
+	}
+}
